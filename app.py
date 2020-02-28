73cd3ec648588b0e248729983dc6a70c81af26b7
Welton <51167205+Welt0n@users.noreply.github.com>
2020-02-28 15:06:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,61 +1,30 @@
 from flask import Flask, request
-from consumer import get_index, ObservedConsumer, ForecastConsumer
+from graphqlConsumer import *
+from flask_graphql import GraphQLView
+
+
 
 app = Flask(__name__)
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
-@app.route('/observed', methods=['GET'])
-def observed_request():
-
-    location_required_args = [['station'], ['latitude', 'longitude']]
-    period_required_args = [['init_date', 'final_date'], ['days']]
-    # Check if required arguments are missing using iterator
-    if not any(all(j in request.args for j in i) for i in location_required_args) or not any(all(j in request.args for j in i) for i in period_required_args):
-
-
-        return " ERROR - Missing required arguments "
-
-    index = get_index(request.args.get('station'), request.args.get('latitude'), request.args.get('longitude'))
-
-    if index is not None:
-
-        consumer = ObservedConsumer(index, request.args.get('init_date'), request.args.get('final_date'), request.args.get('days'))
-
-        if consumer.status:
-            return consumer.json()
-        else:
-            return "ERROR - Invalid arguments for period required"
-
+@app.route('/')
+def input_handler():
+    if request.args.get("query"):
+        return schema.execute(request.args.get("query"))
     else:
-        return "ERROR - Invalid arguments for station or latitude/longitude"
+        return """ Query Example \n{stations(first:2, latitude:-20, longitude:-10) {
+                    name
+                    location
+                    forecast(initdate:"2019-09-11" finaldate:"2019-09-12" ){
+                        periods
+                        maxTemperature
+                        precipitation
+                        }
+                    }
 
-
-@app.route('/forecast', methods=['GET'])
-def forecast_request():
-
-    location_required_args = [['station'], ['latitude', 'longitude']]
-    period_required_args = [['init_date', 'final_date'], ['days']]
-
-    if not any(all(j in request.args for j in i) for i in location_required_args) or not any(all(j in request.args for j in i) for i in period_required_args):
-        # Check if required arguments are missing using iterator
-
-        return " ERROR - Missing required arguments "
-
-    index = get_index(request.args.get('station'), request.args.get('latitude'), request.args.get('longitude'))
-
-    if index:
-
-        consumer = ForecastConsumer(index, request.args.get('init_date'), request.args.get('final_date'), request.args.get('days'))
-        if consumer.status:
-            return consumer.json()
-        else:
-            return "ERROR - Invalid arguments for period required"
-
-    else:
-        return "ERROR - Invalid arguments for station or latitude/longitude"
+                }"""
 
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
